5a91d6de55a9a8ba38859ae0e90a2b175195a1cc
satoooon8888 <samidare256@gmail.com>
2020-04-08 21:54:17 +0900

    impl gac add

--- commands/add.py
+++ commands/add.py
@@ -1,5 +1,20 @@
 import argparse
+from const_setting import accounts_json_path
+from account import Accounts, Account
+from accounts_stream import AccountsStream
+from utils import CommandFailedError
+import logger
 
 
 def cmd_add(args: argparse.Namespace) -> None:
-	pass
+	stream: AccountsStream = AccountsStream(accounts_json_path)
+	accounts: Accounts = stream.load()
+	name: str = input("git user.name: ")
+	if accounts.exist(name):
+		logger.error("This name is already used.")
+		raise CommandFailedError()
+	email: str = input("git user.email: ")
+	account: Account = Account(name, email)
+	accounts.add(account)
+	stream.save(accounts)
+	logger.info("Saved Successfully")
--- main.py
+++ main.py
@@ -63,5 +63,6 @@ def main() -> None:
 		parser.print_help()
 		sys.exit(1)
 
+
 if __name__ == "__main__":
 	main()
